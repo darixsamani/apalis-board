7c0291867a62b740615a19b16cdfadeae28aee46
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-07-12 16:50:59 +0100

    ad features salvo

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,9 @@ pub mod actix;
 #[cfg(all(feature = "axum", not(target_arch = "wasm32")))]
 pub mod axum;
 
+#[cfg(all(feature = "salvo", not(target_arch = "wasm32")))]
+pub mod salvo;
+
+
 #[cfg(all(feature = "web", target_arch = "wasm32"))]
 pub mod web;
